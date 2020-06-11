8d4fd764fe5dcd71bbd96e1d217ca9134f3f4ca9
Gabriel112252 <57492857+Gabriel112252@users.noreply.github.com>
2020-06-11 20:36:24 -0300

    Update and rename QUIZS.js to create.js

---
+++ js/create.js
@@ -0,0 +1,22 @@
+$("#submeter").click(function(){
+    var enunciado = $("#questao").val(),
+    resposta_correta = $("#correta").val(),
+    resposta_incorreta = $("#incorreta").val(),
+    resposta_incorretb = $("#incorretb").val(),
+    resposta_incorretc = $("#incorretc").val(),
+    question = {}
+    var param = {
+        question: {
+                quest:enunciado,
+                correct:resposta_correta,
+                incorrecta:resposta_incorreta,
+                incorrectb:resposta_incorretb,
+                incorrectc:resposta_incorretc
+             }
+    }
+$.post("https://serene-garden-00468.herokuapp.com/questions/",
+        param,
+function(data, status){
+  alert("Data: " + data + "\nStatus: " + status);
+})
+});
